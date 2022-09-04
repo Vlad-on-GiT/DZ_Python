229cc31acee0b23c2ad2327c6e5a2963baa1e144
Владислав <trupackz@gmailcom>
2022-09-04 12:08:55 +0500

    правка

--- seminar_1.py
+++ seminar_1.py
@@ -12,8 +12,7 @@
 # for x in range (2):
 #     for y in range (2):
 #         for z in range (2):
-#             print(x,y,z)
-#             print(not (x or y or z) == (not x) and (not y) and (not z))
+#             print(not (x or y or z) == (not x and not y and not z))
 #__________________________________________________________
 
 # Номер четверти:
